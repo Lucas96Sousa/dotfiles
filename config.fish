3fd704ea517499a69e525b6eaa91a406bfb9cf9b
Lucas Sousa <lucas996oliveira@gmail.com>
2024-03-03 04:55:09 -0300

    Inclusão do go no path do config.fish

--- config.fish
+++ config.fish
@@ -1,12 +1,16 @@
 if status is-login
    set -gx PATH $PATH ~/.cargo/bin
+   set -x -U GOPATH $HOME/go
    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-	end
+
+    alias s0='shutdown -h +0'
+    alias nv='nvim'
+ end
 
 if status is-interactive
     and not set -q TMUX
     zoxide init fish | source
     exec tmux
     alias s0='shutdown -h +0'
+    alias nv='nvim'
 end
-
